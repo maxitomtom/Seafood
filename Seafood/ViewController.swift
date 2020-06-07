19b2389d6f2c76103996468ef184302657702519
Max Thomas <maxitomtom@hotmail.co.uk>
2020-06-07 18:34:43 +0100

    Implementing Inception V3 Model

--- Seafood/ViewController.swift
+++ Seafood/ViewController.swift
@@ -29,13 +29,42 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickedImage = info[.originalImage] as? UIImage {
             imageView.image = userPickedImage
-            print("Image selected")
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            
+            detect(image: ciImage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
     }
 
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading Core ML Model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Data model request failed")
+            }
+            
+            print(results)
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try! handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
+    }
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
         present(imagePicker, animated: true, completion: nil)
